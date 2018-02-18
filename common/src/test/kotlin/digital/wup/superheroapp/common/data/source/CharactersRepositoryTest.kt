d95dca2597f370c9b465479249c0d28a8efe828c
Balazs Varga <warnyul@gmail.com>
2018-02-18 21:20:26 +0100

    Add a repository test

--- common/src/test/kotlin/digital/wup/superheroapp/common/data/source/CharactersRepositoryTest.kt
+++ common/src/test/kotlin/digital/wup/superheroapp/common/data/source/CharactersRepositoryTest.kt
@@ -1,7 +1,63 @@
 package digital.wup.superheroapp.common.data.source
 
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import com.nhaarman.mockito_kotlin.eq
+import digital.wup.superheroapp.common.characters.domain.model.Character
+import digital.wup.superheroapp.common.characters.domain.model.Page
+import digital.wup.superheroapp.common.datasource.CharactersDataSource
+import digital.wup.superheroapp.common.datasource.CharactersRepository
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations.initMocks
+
 /**
  * @author Balazs Varga
  */
 class CharactersRepositoryTest {
+
+    private val characters = listOf<Character>()
+    private val page = Page()
+    @Mock private lateinit var localDataSource: CharactersDataSource
+    @Mock private lateinit var remoteDataSource: CharactersDataSource
+    @Mock private lateinit var successCallback: (List<Character>) -> Unit
+    @Mock private lateinit var errorCallback: () -> Unit
+    private lateinit var charactersRepository: CharactersDataSource
+    private val successCaptor = argumentCaptor<(List<Character>) -> Unit>()
+    private val errorCaptor = argumentCaptor<() -> Unit>()
+
+    @Before
+    fun setupRepository() {
+        initMocks(this)
+        charactersRepository = CharactersRepository(localDataSource, remoteDataSource)
+    }
+
+    @Test
+    fun loadCharacters_repositoryCachesAfterFirstApiCall() {
+
+        twoLoadCharactersCallsToRepository(page, successCallback, errorCallback)
+
+        verify(remoteDataSource).loadCharacters(eq(page), any(), any())
+    }
+
+    private fun twoLoadCharactersCallsToRepository(page: Page,
+                                                   success: (List<Character>) -> Unit,
+                                                   error: () -> Unit) {
+
+        charactersRepository.loadCharacters(page, success, error)
+
+        verify(localDataSource)
+                .loadCharacters(eq(page), any(), errorCaptor.capture())
+
+        errorCaptor.lastValue.invoke()
+
+        verify(remoteDataSource)
+                .loadCharacters(eq(page), successCaptor.capture(), any())
+
+        successCaptor.lastValue.invoke(characters)
+
+        charactersRepository.loadCharacters(page, success, error)
+    }
 }
