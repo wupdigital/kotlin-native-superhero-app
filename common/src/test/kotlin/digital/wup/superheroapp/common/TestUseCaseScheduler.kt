554fb723b2b85e96ae3614363128cf41df5ccf66
Balazs Varga <warnyul@gmail.com>
2018-02-18 20:05:31 +0100

    Add test usecase scheduler

---
+++ common/src/test/kotlin/digital/wup/superheroapp/common/TestUseCaseScheduler.kt
@@ -0,0 +1,19 @@
+package digital.wup.superheroapp.common
+
+/**
+ * @author Balazs Varga
+ */
+class TestUseCaseScheduler: UseCaseScheduler {
+
+    override fun execute(runnable: () -> Unit) {
+        runnable()
+    }
+
+    override fun <Rs> notifyResponse(success: (Rs) -> Unit, response: Rs) {
+        success(response)
+    }
+
+    override fun notifyError(error: () -> Unit) {
+        error()
+    }
+}
